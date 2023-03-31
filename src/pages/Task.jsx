a2acc0073728bfafb1d5155da428d8aaf95a8047
JaydenPears <vladimir120905@gmail.com>
2023-03-31 23:47:46 +0300

    Update page of task

--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -14,6 +14,10 @@ const Task = () => {
 
     const TITLE = `Тестирование № ${task_id}`;
 
+    const func = (e) => {
+        console.log(e.target.getAttribute('a-key'));
+    }
+
     React.useEffect(() => {
         axios.get(
             `http://127.0.0.1:8000/api/test5kl/${task_id}/`
@@ -31,21 +35,33 @@ const Task = () => {
             </Helmet>
             <h1>Task № {task_id}</h1>
             <div className="Tasks">
-                <div className="card" key={1}>
-                    <div className="content">
-                        <div className="NamingOfQuestion">
-                            <h3>{ 'Задание № 1. Прототип из базы № 15.' }</h3>
-                        </div>
-                        <div className="description">
-                            <p align="justify">
-                            Логическая функция F задаётся выражением (w ∨ ¬x) ∧ (w ≡ ¬y) ∧ (w → z). На рисунке приведён частично заполненный фрагмент таблицы истинности функции F, содержащий неповторяющиеся строки. Определите, какому столбцу таблицы истинности функции F соответствует каждая из переменных x, y, z, w.
-                            В ответе напишите буквы x, y, z, w в том порядке, в котором идут соответствующие им столбцы (сначала  — буква, соответствующая первому столбцу; затем  — буква, соответствующая второму столбцу, и т. д.). Буквы в ответе пишите подряд, никаких разделителей между буквами ставить не нужно.
-Пример. Пусть задано выражение x → y, зависящее от двух переменных x и y, и фрагмент таблицы истинности:
-Тогда первому столбцу соответствует переменная y, а второму столбцу соответствует переменная x. В ответе нужно написать: yx.
-                            </p>
+            {tasks.map((_, index) =>
+                    <div className="card" key={index}>
+                        <div className="content">
+                            <div className="NamingOfQuestion">
+                                <h3>{ `Задание № ${index + 1}. Прототип № ${tasks[index]["id"]} из банка заданий.` }</h3>
+                            </div>
+                            <div className="description">
+                                <p align="justify">
+                                    { tasks[index]['description'] }
+                                </p>
+                            </div>
+                            <div className="inputLayout">
+                                <input
+                                    a-key={index}
+                                    type="text"
+                                    className="input-answer"
+                                    required="required"
+                                    onChange={(e) => func(e)}
+                                    />
+                                <span>
+                                    Ответ
+                                </span>
+                            </div>
+                            
                         </div>
                     </div>
-                </div>
+                )}
             </div>
             {/* {tasks.map((_, index) =>
                 <h1>1</h1>
