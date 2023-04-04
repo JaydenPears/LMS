e78c803b90276aefa0f8c1b0ca2bcae2dc38df86
JaydenPears <vladimir120905@gmail.com>
2023-04-04 17:57:47 +0300

    Add button for push another answer again for test at results page

--- src/pages/ResultOfTest.jsx
+++ src/pages/ResultOfTest.jsx
@@ -2,11 +2,14 @@
 import React from "react"
 import { useLocation } from "react-router";
 import { Helmet } from 'react-helmet';
+import { Link } from "react-router-dom";
 
 // import styles:
 import "../static/styles/ResultOfTest.css"
 
 const ResultOfTest = () => {
+    const prev_location = useLocation().pathname.split('/').
+    slice(0, useLocation().pathname.split('/').length - 1).join('/');
     const {state} = useLocation();
     const { test_id, show_results, text } = state;
 
@@ -18,6 +21,7 @@ const ResultOfTest = () => {
                 <title>{TITLE}</title>
             </Helmet>
             <h1>{ text }</h1>
+            <Link className="PushAnotherAnswer" to={prev_location}>Отправить ещё один ответ</Link>
         </div>
     )
 };
