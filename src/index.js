0d94c1a5b572415cee1305ae4beee411cab13a8f
JaydenPears <vladimir120905@gmail.com>
2023-04-18 23:11:07 +0300

    Add favicon

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import {BrowserView, MobileView} from 'react-device-detect';
+import Favicon from 'react-favicon';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<BrowserRouter>
+		<Favicon url="static/favicon.ico"/>
 		<BrowserView>
 			<App />
 		</BrowserView>
